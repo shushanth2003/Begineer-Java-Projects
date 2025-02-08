982ca4638879f8a081cf353a56e00cfa84756c46
shushanth2003 <bsshu2003@gmail.com>
2025-02-08 17:07:28 +0530

    "Feat: Add The File"

--- bankingsystem/src/Deposit.java
+++ bankingsystem/src/Deposit.java
@@ -1,2 +1,14 @@
-public class Deposit {
+import java.util.Scanner;
+class Deposit {
+    String option;
+    int amount;
+    Deposit(){
+        this.option=option;
+        this.amount=amount;
+    }
+    void getDeposit(){
+        System.out.println("Enter the amount");
+        Scanner in = new Scanner(System.in);
+        amount=in.nextInt();
+    }
 }
---
+++ bankingsystem/src/Withdraw.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Withdraw extends Deposit{
+
+    Withdraw(String option){
+        super();
+
+    }
+    void withdraw(){
+        System.out.println("Enter the amount from savings");
+
+    }
+}
